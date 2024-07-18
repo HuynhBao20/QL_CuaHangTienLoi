ac8b597f4f554f590a406d4c89ee529cb490799d
HuynhBao20 <HuynhBao20>
2024-07-18 20:33:59 +0700

    create project

---
+++ APP/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ConnectionDB;
+namespace APP
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+			Connection db = new Connection();
+			dataGridView1.DataSource = db.loadDB("SELECT * FROM LOAISP");
+			loadSP();
+		}
+		public void loadSP()
+		{
+			for(int i = 0; i < 5; i++)
+			{
+				Panel pnl = new Panel()
+				{
+					Width = 150,
+					Height = 180,
+					BackColor = Color.Red
+				};
+				Button btn = new Button()
+				{
+					Name = "btn",
+					TextAlign = ContentAlignment.BottomCenter,
+					Width = 150,
+					Height = 150,
+
+				};
+				Label lb = new Label();
+				
+				lb.Text = "Sản phẩm 1";
+				pnl.Controls.Add(btn);
+				pnl.Controls.Add(lb);
+				flowLayoutPanel1.Controls.Add(pnl);
+			}
+			
+		}
+	}
+}
---
+++ ConnectionDB/Connection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace ConnectionDB
+{
+	public class Connection
+	{
+		public static string _conStr = @"Data Source=LAPTOP-V4M3UK0D\THEBAO;Initial Catalog=QL_CHTienLoi;Integrated Security=True";
+		SqlConnection _cnn = new SqlConnection(_conStr);
+		public DataTable loadDB(string Sql)
+		{
+			DataTable da = new DataTable();
+			SqlDataAdapter sda = new SqlDataAdapter(Sql, _cnn);
+			sda.Fill(da);
+			return da;
+		}
+	}
+}
