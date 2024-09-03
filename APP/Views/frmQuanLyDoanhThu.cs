2cf68b83788034d742ee07a3ae53a0c7a0481f75
HuynhBao20 <HuynhBao20>
2024-09-04 06:16:08 +0700

    update 4/9

--- APP/Views/frmQuanLyDoanhThu.cs
+++ APP/Views/frmQuanLyDoanhThu.cs
@@ -1,4 +1,5 @@
 ﻿using APP.Controllers;
+using ConnectionDB;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -14,13 +15,17 @@ namespace APP.Views
 	public partial class frmQuanLyDoanhThu : Form
 	{
 		Analyst a = new Analyst();
-
+		Connection db = new Connection();
 		public frmQuanLyDoanhThu()
 		{
 			InitializeComponent();
 			a.Analyst_Month(chart1);
 			a.Analyst_Product_Buy(chart2);
-
+			int currentMonth = DateTime.Now.Month;
+			label7.Text = int.Parse(db.ExcuteReader($"EXEC sp_ThongKeTheoThang {currentMonth}", "Tổng giá trị hóa đơn")).ToString("0,00") + " VNĐ";
+			label6.Text = float.Parse(db.ExcuteReader($"SELECT AVG(SOLUONG * DONGIA) AS 'DT' FROM HOADON, SANPHAM, CT_HOADON WHERE HOADON.MAHD = CT_HOADON.MAHD AND SANPHAM.MASP = CT_HOADON.MASP", "DT")).ToString("0,00") + " VNĐ";
+			label8.Text = int.Parse(db.ExcuteReader($"SELECT COUNT(*) AS 'SL' FROM HOADON", "SL")).ToString();
+			//label5.Text = int.Parse(db.ExcuteReader($"SELECT SUM(*) AS 'SL' FROM HOADON", "SL")).ToString("0.00") + " VNĐ";
 		}
 	}
 }
